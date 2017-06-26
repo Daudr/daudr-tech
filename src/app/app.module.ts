8352369b68c39e5f96d335fa4efbb07d9e5a5a1c
Michele Da Rin <michidarin@gmail.com>
2017-06-26 21:16:27 +0200

    Aggiunte routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { TestComponent } from './components/test/test.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent },
+  { path: 'test', component: TestComponent }/*,
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'musica', component: MusicaComponent }*/
+];
 
 @NgModule({
   declarations: [
